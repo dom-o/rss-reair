8749f44a32f76dc13338b66dc004772143ac8e54
Domonic Milesi <dom-o@users.noreply.github.com>
2021-08-02 16:25:27 -0400

    Minor fix.

--- rss_restart/manage.py
+++ rss_restart/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rss_restart.settings.base')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rss_restart.settings1111.base')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
